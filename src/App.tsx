75267bace38dacd3e429a56038c884316876013b
Efimov Yaroslav <ya.efimov@internet.ru>
2024-05-09 20:08:29 +0300

    + sc props

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import './App.css';
-import styled from 'styled-components';
+import "./App.css";
+import styled from "styled-components";
 
-import {StyledBtn, SupetButton} from './components/Button.styled';
-import {Link} from './components/Link.styled';
-import {Menu} from './components/Menu.styled';
+import { StyledBtn, SupetButton } from "./components/Button.styled";
+import { Link } from "./components/Link.styled";
+import { Menu } from "./components/Menu.styled";
 
 function App() {
   return (
     <div className="App">
-      <Menu>
+      {/* <Menu>
         <ul>
-          <li><a href="">menu item 1</a></li>
-          <li><a href="">menu item 2</a></li>
-          <li><a href="">menu item 3</a></li>
+          <li>
+            <a href="">menu item 1</a>
+          </li>
+          <li>
+            <a href="">menu item 2</a>
+          </li>
+          <li>
+            <a href="">menu item 3</a>
+          </li>
         </ul>
       </Menu>
       <Box>
-        <StyledBtn as={Link} href='#'>LinkComponent</StyledBtn>
-        <StyledBtn as='a' href='#'>Link</StyledBtn>
-        <StyledBtn>Hello</StyledBtn>
-        <SupetButton>Super button</SupetButton>
-      </Box>    
+        <StyledBtn as={Link} href="#">
+          LinkComponent
+        </StyledBtn>
+        <StyledBtn as="a" href="#">
+          Link
+        </StyledBtn> */}
+      <Box>
+        {/* <StyledBtn color="Green" fontSize={"20px"}>
+          Hello
+        </StyledBtn>
+        <StyledBtn color="Red">Hello</StyledBtn> */}
+        <StyledBtn btntype={"primary"} active>
+          Hello
+        </StyledBtn>
+        <StyledBtn btntype={"outlined"}>Hello</StyledBtn>
+        {/* <SupetButton>Super button</SupetButton> */}
+      </Box>
     </div>
   );
 }
@@ -39,14 +57,12 @@ const Box = styled.div`
   }
 
   // обращение к компоненте Link
-  ${Link}{
+  ${Link} {
     cursor: zoom-in;
   }
-  
+
   // после 800 px элементы вертикальные
   @media screen and (max-width: 800px) {
     flex-direction: column;
   }
-
-
-`
+`;
--- src/components/Button.styled.tsx
+++ src/components/Button.styled.tsx
@@ -1,34 +1,74 @@
-import styled from "styled-components"
+import styled, { css } from "styled-components";
 
-import { MyAnimation } from "../styles/animation/Animations"
+import { MyAnimation } from "../styles/animation/Animations";
 
-export const StyledBtn = styled.button`
+type StyledBtnPropsType = {
+  color?: string;
+  fontSize?: string;
+  btntype: "primary" | "outlined";
+  active?: boolean;
+  // primary?: boole an;
+  // outlined?: boolean;
+};
+
+export const StyledBtn = styled.button<StyledBtnPropsType>`
   border: 0cm;
-  background-color: #86182e;
+  /* background-color: #86182e; */
+  background-color: ${(props) => props.color || "#86182e"}; // по умолчанию
   padding: 10px 20px;
   //border-radius: 5px;
   color: snow;
-  font-size: 2rem;
+  //font-size: 2rem;
+  font-size: ${(props) => props.fontSize || "2rem"};
   font-weight: bold;
+  border-radius: 10px;
 
-  // same & = button
-  &:hover {
-    background-color: aqua;
-  }
-
-  &:last-child{
+  /* &:last-child {
     background-color: green;
-  }
+  } */
 
-`
+  //outlined
+  ${(props) =>
+    props.btntype === "outlined" &&
+    css<StyledBtnPropsType>`
+      border: 2px solid ${(props) => props.color || "#86182e"};
+      color: ${(props) => props.color || "#86182e"};
+      background-color: transparent;
+
+      &:hover {
+        border-color: aqua;
+        color: aqua;
+        background: transparent;
+      }
+    `}
+
+  //primary
+    ${(props) =>
+    props.btntype === "primary" &&
+    css<StyledBtnPropsType>`
+      background-color: ${(props) => props.color || "#86182e"};
+      color: snow;
+
+      // same & = button
+      &:hover {
+        background-color: aqua;
+      }
+    `}
+
+    ${(props) =>
+    props.active &&
+    css<StyledBtnPropsType>`
+      box-shadow: 5px 5px 5px 5px gray;
+    `}
+`;
 
 // РАСШИРЕНИЕ На Базе StyledBtn
 export const SupetButton = styled(StyledBtn)`
   border-radius: 5px;
   color: black;
   background-color: yellow;
-  
-  &:hover{
+
+  &:hover {
     animation: ${MyAnimation} 2s infinite; // добавляем анимацию
   }
-`
+`;
